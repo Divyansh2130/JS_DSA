342baf8c2fffcecacb6fc74eaf55fcfe68202141
Divyansh2130 <123561213+Divyansh2130@users.noreply.github.com>
2026-01-29 14:53:05 +0530

    Time: 314 ms (5.14%) | Memory: 64.8 MB (5.14%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.ts
@@ -0,0 +1,38 @@
+function asteroidCollision(asteroids: number[]): number[] {
+    let stack:number[]=[];
+
+    for(let num of asteroids){
+        if(stack.length==0){stack.push(num);continue;}
+
+        let top=stack[stack.length - 1];
+        let cur=num;
+
+        while(stack.length!=0){
+            console.log("top:",top);
+            console.log("cur:",cur);
+            if(top*cur>0){
+                stack.push(cur); break;
+            }
+            else if(top<0){
+               stack.push(cur); break; 
+            }
+            else if(Math.abs(top)==Math.abs(cur)){
+                stack.pop();break;
+            }
+            else{
+                if(Math.abs(top)>Math.abs(cur))break;
+                else{
+                    stack.pop();
+                    if(stack.length==0){
+                        stack.push(cur);
+                        break;
+                    }
+                    top=stack[stack.length - 1];
+                }
+            }
+
+        }        
+    }
+
+    return stack;
+};
