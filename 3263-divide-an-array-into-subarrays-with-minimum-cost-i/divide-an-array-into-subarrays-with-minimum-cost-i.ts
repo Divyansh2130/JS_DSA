72baa676e62161950f1cf2c032f3aceb0c1956e6
Divyansh2130 <123561213+Divyansh2130@users.noreply.github.com>
2026-02-02 10:09:58 +0530

    Time: 2 ms (37.50%) | Memory: 57.8 MB (75.00%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.ts
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.ts
@@ -1,8 +1,8 @@
 function minimumCost(nums: number[]): number {
     let fElement=nums[0];
     const start = 1;
-const end = nums.length - 1; // or any end >= start
-nums.splice(start, end - start + 1, ...nums.slice(start, end + 1).sort((a, b) => a - b));
+    const end = nums.length - 1; // or any end >= start
+    nums.splice(start, end - start + 1, ...nums.slice(start, end + 1).sort((a, b) => a - b));
 
 
     return fElement+nums[1]+nums[2];
