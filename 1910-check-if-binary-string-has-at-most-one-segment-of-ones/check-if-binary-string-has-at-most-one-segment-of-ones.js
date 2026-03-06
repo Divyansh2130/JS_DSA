e78c2aa5fdecf6b3ac99fc636cf1ada15104e9b6
Divyansh2130 <123561213+Divyansh2130@users.noreply.github.com>
2026-03-06 17:51:38 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (66.67%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -2,13 +2,4 @@
  * @param {string} s
  * @return {boolean}
  */
-var checkOnesSegment = function(s) {
-    let flag=false;
-    for(let i=1;i<s.length;i++){
-        if(s[i]=='0')flag=true;
-
-        if(flag && s[i]=='1')return false;
-    }
-
-    return true;
-};
+const checkOnesSegment = s => !s.includes(`01`);
