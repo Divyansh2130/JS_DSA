8aea06707608acfdaeb930aa9d91b8b33170c80d
Divyansh2130 <123561213+Divyansh2130@users.noreply.github.com>
2026-04-30 11:48:06 +0530

    Time: 1060 ms (70.00%) | Memory: 89.1 MB (60.00%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.js
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.js
@@ -1,28 +1,26 @@
-function recursion(grid, k, i, j, dp) {
-
-    if (i >= grid.length || j >= grid[0].length) return -1;
-
+function recursion(grid,k,i,j,dp) {
+    if (i>=grid.length || j>=grid[0].length) return -1;
     if (k < 0) return -1;
 
-    if (i === grid.length - 1 && j === grid[0].length - 1) {
-        if (grid[i][j] > 0) k--;
-        return k < 0 ? -1 : grid[i][j];
+    if (i===grid.length-1 && j===grid[0].length-1) {
+    if (grid[i][j] > 0) k--;
+        if(k<0)return -1;
+        else return grid[i][j];
     }
-
     if (dp[i][j][k] !== -2) return dp[i][j][k];
 
-    let newK = k;
-    if (grid[i][j] > 0) newK--;
+    let nK = k;
+    if (grid[i][j] > 0) nK--;
 
-    let right = recursion(grid, newK, i, j + 1, dp);
-    let down  = recursion(grid, newK, i + 1, j, dp);
+    let rightScore = recursion(grid,nK,i,j+1,dp);
+    let downScore  = recursion(grid,nK,i+1,j,dp);
 
-    let best = Math.max(right, down);
+    let best = Math.max(rightScore, downScore);
 
     if (best === -1) {
         dp[i][j][k] = -1;
     } else {
-        dp[i][j][k] = grid[i][j] + best;
+        dp[i][j][k]=grid[i][j]+best;
     }
 
     return dp[i][j][k];
