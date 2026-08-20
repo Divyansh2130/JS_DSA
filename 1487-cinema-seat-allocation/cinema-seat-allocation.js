d7daa16deb754888159b8202431d8ed0374f8fc5
Divyansh2130 <123561213+Divyansh2130@users.noreply.github.com>
2026-08-20 11:43:06 +0530

    Time: 11 ms (93.10%) | Memory: 63.7 MB (86.21%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @param {number[][]} reservedSeats
+ * @return {number}
+ */
+var maxNumberOfFamilies = function(n, reservedSeats) {
+    const left= 0b11110000;
+    const right=0b00001111;
+    const middle=0b11000011;
+
+    const occupied=new Map();
+    for(const seat of reservedSeats){
+        if(seat[1]>=2 && seat[1]<=9){
+            const row=seat[0];
+            if(!occupied.has(row)){
+                occupied.set(row,0);
+            }
+            occupied.set(row,occupied.get(row) | (1<<(seat[1]-2)));
+        }
+    }
+
+    let ans=(n-occupied.size)*2;
+    for(const bitmask of occupied.values()){
+        if((bitmask | left) ===left ||(bitmask | middle)===middle || (bitmask | right)===right ){
+            ans++;
+        }
+    }
+    return ans;
+};
